03e636e49304d2a6de345e8e130074f3f6778259
linzigeer <263720492@qq.com>
2024-04-04 15:29:18 +0800

    finish read/print/csv, and convert to json and write to file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
-fn main() {
-    println!("Hello, world!");
+use anyhow::Result;
+use clap::Parser;
+
+use csv_cli::{Opts, SubCommand};
+
+use crate::cli::process_csv;
+
+mod cli;
+
+fn main() -> Result<()> {
+    let opts = Opts::parse();
+    println!("{:?}", opts);
+
+    println!("{:?}", std::env::current_dir());
+
+    match opts.cmd {
+        SubCommand::Csv(opts) => process_csv(
+            &opts.input,
+            &opts.output.unwrap_or("./output.json".to_string()),
+        )?,
+    }
+
+    Ok(())
 }
